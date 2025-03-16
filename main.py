b2482e9b674d6d723474db8417987bec29c796e8
aabouqas42 <abouqassimeledrissi@gmail.com>
2025-03-16 18:08:15 +0000

    done

---
+++ main.py
@@ -0,0 +1,138 @@
+#!.venv/bin/python3
+import random
+
+import pygame
+from pygame import *
+
+pygame.init
+
+WIDTH = 640
+HEIGHT = 920
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("PING PONG")
+
+running = True
+
+colors = [pygame.Color('red'), pygame.Color('blue'), pygame.Color('pink'), pygame.Color('yellow')]
+# WIDTH / 2, HEIGHT - 11
+i = 0
+
+
+
+PADDLE_WIDTH = 100
+PADDLE_HEIGHT = 10
+# player = [[WIDTH / 2, HEIGHT], [WIDTH / 2, HEIGHT - 10], ]
+
+
+LEFT = 1
+RIGHT = 2
+BALL_SIZE = 15
+playerMovmentSpeed = 0.5
+
+class Player:
+    x = 0
+    y = 0
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def getCourdinates(self):
+        return [self.x, self.y]
+class Ball:
+    x = 0
+    y = 0
+    speed = 1
+    directionX = 0
+    directionY = 1
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def increace(self):
+        self.x += self.directionX
+        self.y += self.directionY
+    def getCourdinates(self):
+        return [self.x, self.y]
+
+player = Player(WIDTH // 2 - (PADDLE_WIDTH // 2), HEIGHT - (PADDLE_HEIGHT // 2))
+ball = Ball(WIDTH / 2, HEIGHT / 2)
+
+def movePlayer():
+    if (keyPressed == K_LEFT and player.x == 0) or (keyPressed == K_RIGHT and (player.x + PADDLE_WIDTH)== WIDTH):
+        return
+    if keyPressed == K_LEFT:
+        player.x -= playerMovmentSpeed
+    if keyPressed == K_RIGHT:
+        player.x += playerMovmentSpeed
+
+key_hold = True
+keyPressed = 0
+
+gravity = 0.1
+
+W_BOTTOM = HEIGHT - PADDLE_HEIGHT
+W_RIGHT = WIDTH - BALL_SIZE
+
+while running:
+    screen.fill(Color("black"))
+    if i % ball.speed == 0:
+
+        ball.increace()
+
+        if ball.y >= W_BOTTOM or ball.y < BALL_SIZE:
+            ball.directionY *= -1
+
+        if ball.x >= W_RIGHT or ball.x <= BALL_SIZE:
+            ball.directionX *= -1
+
+        if ball.y == W_BOTTOM and ball.directionX == 0:
+            ball.directionX = 1
+
+    if ball.y == W_BOTTOM:
+        if ball.x < player.x:
+            running = False
+        if  ball.x > player.x + 100:
+            running = False
+        # if ball.y == (HEIGHT - 20) and ball_courd[0] < player.x[0]:
+    #     running = False
+    #     if player.x < ball.x < player.x + PADDLE_WIDTH:
+    #         if player.x < ball.x < (player.x + PADDLE_WIDTH // 2):
+    #             ball.directionX *= -1
+    #         elif (player.x + PADDLE_WIDTH // 2) < ball.x < player.x + PADDLE_WIDTH:
+    #             ball.directionX *= 1
+    #         else:
+    #             ball.directionX = 0
+
+        if player.x < ball.x < player.x + PADDLE_WIDTH:
+            relative_impact = ((ball.x - player.x) / PADDLE_WIDTH) * 2 - 1
+            ball.directionX = relative_impact
+        ball.directionY *= -1
+        ball.speed -= 1
+    else:
+        ball.speed = 2
+
+    pygame.draw.circle(screen, pygame.Color("red"), ball.getCourdinates(), BALL_SIZE)
+    pygame.draw.rect(screen, pygame.Color("red"), pygame.Rect(player.x, player.y, PADDLE_WIDTH, PADDLE_HEIGHT))
+
+    for envent in pygame.event.get():
+        if envent.type == pygame.QUIT:
+            running = False
+            break
+        elif envent.type == pygame.KEYDOWN:
+            key_hold = True
+            keyPressed = envent.key
+            if envent.key == pygame.K_ESCAPE:
+                running = False
+        elif envent.type == pygame.KEYUP:
+            key_hold = False
+
+    if key_hold:
+        if keyPressed == K_RIGHT or keyPressed == K_LEFT:
+            movePlayer()
+
+    pygame.display.flip()
+    i += 1
+    if i == 1000:
+        i = 0
+
+print("End")
+pygame.quit()
